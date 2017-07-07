0f09905cdbf5a9f38e1ecf28749f32793689db8f
DWCarruth <daniel.carruth@gmail.com>
2017-07-06 23:40:59 -0500

    Initial commit
    Initial commit of Formula SAE Hololens application to Github

---
+++ Assets/Scripts/ShowHideAction.cs
@@ -0,0 +1,33 @@
+﻿using Academy.HoloToolkit.Unity;
+using UnityEngine;
+
+/// <summary>
+/// InteractibleAction performs custom actions when you gaze at the holograms.
+/// </summary>
+public class ShowHideAction : MonoBehaviour
+{
+    [Tooltip("Drag the descripion window asset you want to display.")]
+    public GameObject DescriptionWindow;
+    public Texture slide;
+
+    void PerformTagAlong()
+    {
+        if (DescriptionWindow == null)
+        {
+            return;
+        }
+        
+        // display window
+        Renderer rend = DescriptionWindow.GetComponent<Renderer>();
+        if(!rend.isVisible)
+        {
+            rend.enabled = true;
+        }
+
+        // update texture
+        if(slide != null)
+        {
+            rend.material.mainTexture = slide;
+        }
+    }
+}
